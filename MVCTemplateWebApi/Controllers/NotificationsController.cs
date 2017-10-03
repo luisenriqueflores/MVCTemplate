86b7043d8f49332b19eccb185fe6ccadcaa2c375
luisenriqueflores <luis.enrique.flores@outlook.com>
2017-10-03 12:21:11 -0500

    Adding API

---
+++ MVCTemplateWebApi/Controllers/NotificationsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MVCTemplateWebApi.Controllers
+{
+    public class NotificationsController : ApiController
+    {
+        public List<string> Get()
+        {
+            return new List<string>() { "Notification 1", "Notification 2" };
+        }
+    }
+}
